3f1dac2208a0b2d7ead18ef182a88d888f39c327
yucel-bozdogan <yucel.bozdogan@outlook.com>
2025-10-17 00:48:01 +0300

    add dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Providers from "./Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,11 +20,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Providers> 
         <Header/>  
         {children}
+        </Providers> 
       </body>
     </html>
   );
---
+++ src/components/DarkModeSwitch.jsx
@@ -0,0 +1,27 @@
+"use client"
+
+import {MdLightMode,MdDarkMode} from 'react-icons/md'
+import React, { useEffect, useState } from 'react'
+import { useTheme } from 'next-themes'
+export default function DarkModeSwitch() {
+    const {theme,setTheme,systemTheme} = useTheme();
+    const[mounted,setMounted] = useState(false);
+    const currentTheme = theme ==='system' ? systemTheme:theme
+    useEffect(() => setMounted(true), []);
+  return (
+    <div>
+        {
+            mounted && (currentTheme === 'dark' ?(
+            <MdLightMode 
+            onClick={()=>setTheme('light')} 
+            className='text-xl cursor-pointer hover:text-amber-500'
+            />
+            ):(
+            <MdDarkMode 
+            onClick={()=>setTheme('dark')}  
+            className='text-xl cursor-pointer hover:text-amber-500'
+            />
+        ))}
+    </div>
+  )
+}
