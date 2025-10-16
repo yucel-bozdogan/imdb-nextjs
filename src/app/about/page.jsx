799977915d8cae89ec1f410070207afaf79121c5
yucel-bozdogan <yucel.bozdogan@outlook.com>
2025-10-17 01:02:28 +0300

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4 '>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>IMDb, historically known as the Internet Movie Database, is an online database of information related to films, television series, podcasts, home videos, video games, and streaming content online – including cast, production crew and biographies, plot summaries, trivia, ratings, and fan and critical reviews. IMDb began as a fan-operated movie database on the Usenet group "rec.arts.movies" in 1990, and moved to the Web in 1993. Since 1998, it has been owned and operated by IMDb.com, Inc., a subsidiary of Amazon.</p>
+
+        <p>The site's message boards were disabled in February 2017. As of 2024, IMDb was the 51st most visited website on the Internet, as ranked by Semrush. As of March 2022, the database contained some 10.1 million titles (including television episodes), 11.5 million person records, and 83 million registered users.</p>
+
+        <p>User profile pages show a user's registration date and, optionally, their personal ratings of titles. Since 2015, "badges" can be added showing a count of contributions. These badges range from total contributions made to independent categories such as photos, trivia, and biographies. If a registered user or visitor is in the entertainment industry and has an IMDb page, they can add photos through IMDbPRO. </p>
+    </div>
+  )
+}
