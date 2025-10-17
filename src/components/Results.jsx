57bc8afc47e0267579bc1b70d2fc3eaf53e3c0c6
yucel-bozdogan <yucel.bozdogan@outlook.com>
2025-10-17 21:59:13 +0300

    add card component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
-
+import Card from './Card'
 export default function Results({results}) {
   return (
-    <div>
-        
-            {results.map((result) => (
-                <div key = {result.id}>
-                    <h2>{result.original_title}</h2>
-                </div> 
-                )) }  
-        
-    </div>
-  );
+    <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 max-w-6xl mx-auto p-4 w-full'>
+      {results.map((result) => (   
+      <Card key ={result.id} result={result}/>
+      ))}  
+    </div> 
+    );
 }
